37add359595ed4a0409238cd8f62dfbf00c5c49d
Suv71 <d.mihalin@yandex.ru>
2018-05-02 23:55:39 +0300

    Создал проект, подключил opencv

---
+++ ChangingFace/ChangingFace/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
+using Microsoft.Win32;
+
+namespace ChangingFace
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            if (openFile.ShowDialog().Value)
+            {
+                Image<Bgr, Byte> myImage = new Image<Bgr, byte>(openFile.FileName);
+                image.Source = ToBitmapSource(myImage);
+            }
+        }
+
+        private BitmapSource ToBitmapSource(IImage image)
+        {
+            using (Bitmap source = image.Bitmap)
+            {
+                IntPtr ptr = source.GetHbitmap();
+
+                var result = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                                ptr,
+                                IntPtr.Zero,
+                                Int32Rect.Empty,
+                                BitmapSizeOptions.FromEmptyOptions());
+                return result;
+            }
+        }
+    }
+}
